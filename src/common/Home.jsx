91d6b51df3792b20bdb22e90caeda58f29862061
Rayan Ahmad <rayan25nov@gmail.com>
2023-06-21 22:00:02 +0530

    Modified Home component

--- src/common/Home.jsx
+++ src/common/Home.jsx
@@ -5,24 +5,32 @@ import {
   faFacebook,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import "./Home.css"; // Import the CSS file for styling
 
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div className="container">
+      <div className="text-section">
         <h1>All Delicious Cuisines</h1>
-        <p>Pizza, Salad, Burger, Pasta, Sandwiches, Desserts, and more</p>
+        <p>
+          Pizza, Salad, Burger, Pasta, Sandwiches, Desserts, Roti Sabzi, Drinks
+          and more
+        </p>
         <button>Find out more</button>
       </div>
-      <div>
-        <img
-          src="../../src/assets/images/pasta.jpg"
-          alt="Pasta on a plate"
-          height={"200px"}
-        />
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      <div className="image-section">
+        <img src="../../src/assets/images/pasta.jpg" alt="Pasta on a plate" />
+        <div className="social-icons">
+          <div className="icon">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </div>
+          <div className="icon">
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </div>
+        </div>
       </div>
     </div>
   );
